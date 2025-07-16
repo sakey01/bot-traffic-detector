e92821f7518dc32c34f05841bb61b2145f401e1d
Sakey01 <sheikh36ahmed@gmail.com>
2025-07-16 18:54:38 +0100

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+# Server Logs Analyser
+
+## Overview
+
+A Python script that scans server logs to identify IP addresses with the most requests. Useful for detecting possible bot traffic and server overload.
+
+## Requirements
+
+- Python 3.8 or higher
+
+## How to Use
+
+1. [Download or clone](https://github.com/sakey01/log-reader) the repository from GitHub  
+2. Place your log file inside the `logs` folder (e.g., `logs/example.log`)  
+3. Open your terminal or command prompt  
+4. Navigate to the root directory of the project:
+
+```bash
+cd path/to/log-reader
+python script.py your-log-name.log
---
+++ script.py
@@ -0,0 +1,38 @@
+import re
+import sys
+import collections
+
+# file setup
+if len(sys.argv) < 2:
+    print("file name missing")
+    sys.exit()
+
+logFile = 'logs/' + sys.argv[1]
+
+# regex to get IP
+regex = re.compile(r"^(\d{1,3}(?:\.\d{1,3}){3})")
+
+# request count setup
+reqCount = collections.Counter()
+
+# parses file to match regex conditions
+with open(logFile, "r") as file:
+    for line in file:
+        match = regex.match(line)
+        if match:
+            ip = match.group(1)
+            reqCount[ip] += 1
+
+# prints IPs with requests over 300
+print("Most Common Requests")
+for i, count in reqCount.most_common(100):
+    if (count > 300):
+        print(f"{i}: {count} requests")
+
+        
+# exports ips to a txt file
+with open('ip_alerts.txt', 'w') as f:
+    f.write("FLAGGED\n")
+    for ip, count in reqCount.items():
+        if count >= 300:
+            f.write(f"{ip} - {count} requests\n")
